5d9500595c5a07a9a31d873aa4c0d4fb675fa8ea
angeld55 <angel_dimitriev@abv.bg>
2021-10-14 13:06:06 +0300

    added solutions

---
+++ Sem. 01/Solutions/task5.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	cout << n / 10 << endl;
+
+}
---
+++ Sem. 02/Solutions/03. Figures.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+const double PI = 3.14;
+int main()
+{
+	char figure;
+	cin >> figure;
+
+	if (figure == 's')
+	{
+		double side;
+		cin >> side;
+		cout << side * side << endl;
+	}
+	else if (figure == 'r')
+	{
+		double sideA, sideB;
+		cin >> sideA >> sideB;
+		cout << sideA * sideB << endl;
+	}
+	else if (figure == 'c')
+	{
+		double R;
+		cin >> R;
+		cout << PI * R * R << endl;
+	}
+}
