218309b4bc02dc94306faf60cd4bb4c73b1e675e
Justsvetoslavov <justsvetoslavov@gmail.com>
2021-10-17 12:41:29 +0300

    Pract. 01 - Solutions

---
+++ pract_SI/pract. 01/task1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+	 string name;
+	 cout << "What is your name?\n";
+	 cin >> name;
+	 cout << "Hello, " << name << endl;
+
+	 return 0;
+}
---
+++ pract_SI/pract. 01/task5.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+#include<iomanip>
+using namespace std;
+int main()
+{
+	 string card_number;;
+
+	 cout << "Card number: ";
+	 cin >> card_number;
+
+	 cout << "Hidden card number: " << setfill('X') << setw(13)
+		  << card_number[12] << card_number[13] << card_number[14] << card_number[15] << endl;
+
+	 return 0;
+}
