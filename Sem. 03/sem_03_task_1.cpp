d98099a27aefb2586a9416f1f66fd3a1e560df1d
TihomirGalov <titi02@mail.bg>
2021-10-28 17:46:14 +0300

    Update sem_03_task_1.cpp
    reformat

--- Sem. 03/sem_03_task_1.cpp
+++ Sem. 03/sem_03_task_1.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main() 
+{
     int sum = 0;
     int n = 1;
-    while (n != 0){
+    
+    while (n != 0)
+    {
         cin >> n;
         sum += n;
     }
+    
     cout << sum << endl;
     return 0;
 }
