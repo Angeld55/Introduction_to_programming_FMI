ca9fbe27514b256ed778f73686a231c8a884bcd1
TihomirGalov <titi02@mail.bg>
2021-10-28 17:26:57 +0300

    solutions to tasks 1 & 2

---
+++ Sem. 03/sem_03_task_2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1, num2;
+    cin >> num1 >> num2;
+    for (int i = num1; i >= 1; i--){
+        if (num1 % i == 0 && num2 % i == 0){
+            cout << i;
+            if (i != 1)cout << " ";
+        }
+    }
+    cout << endl;
+    return 0;
+}
