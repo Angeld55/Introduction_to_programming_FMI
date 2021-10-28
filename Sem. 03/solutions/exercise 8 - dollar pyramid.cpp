616692d1e3605bbc77754c90434b82eb6b41c1d8
Kristian Todorov <63777932+kristian3551@users.noreply.github.com>
2021-10-28 16:45:14 +0300

    solved ex 8 and 9 from sem. 3

--- Sem. 03/solutions/exercise 8 - dollar pyramid.cpp
+++ Sem. 03/solutions/exercise 8 - dollar pyramid.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
 using namespace std;
 
-void printPyramid(int n) {
-    for(int i = 1; i <= n; i++) {
-        for(int j = 1; j <= i; j++) 
-            cout << "$" << " "; // print as many dollars as the number of the current row
-        cout << endl;
-    }
-}
+int main() 
+{
 
-int main() {
     int n;
     cin >> n;
-    printPyramid(n);
+
+    for(int i = 1; i <= n; i++) 
+    {
+        for(int j = 1; j <= i; j++) 
+            cout << "$" << " "; // print as many dollars as the number of the current row
+        
+        cout << endl;
+    }
+    
     return 0;
 }
