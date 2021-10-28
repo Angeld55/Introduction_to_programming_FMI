81d4602f7ba4c38879d5b39df0e64533cc591d54
TihomirGalov <titi02@mail.bg>
2021-10-28 17:47:40 +0300

    reformat

--- Sem. 03/sem_03_task_2.cpp
+++ Sem. 03/sem_03_task_2.cpp
@@ -1,15 +1,21 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main() 
+{
     int num1, num2;
     cin >> num1 >> num2;
-    for (int i = num1; i >= 1; i--){
-        if (num1 % i == 0 && num2 % i == 0){
+    
+    for (int i = num1; i >= 1; i--)
+    {
+        if (num1 % i == 0 && num2 % i == 0)
+        {
             cout << i;
-            if (i != 1)cout << " ";
+            if (i != 1)
+                cout << " ";
         }
     }
+    
     cout << endl;
     return 0;
 }
