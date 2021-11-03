d0071de8895c62003670e95421b5bc11185e69a9
Syndamia <kamen.d.mladenov@protonmail.com>
2021-11-03 17:29:20 +0200

    Fixed spacing incontinuity

--- Sem. 03/Solutions/15. Bowtie.cpp
+++ Sem. 03/Solutions/15. Bowtie.cpp
@@ -7,26 +7,26 @@ int main() {
 	cin >> n;
 
 	char middleSpacer = ' ';
-	for(int i = -n + 1; i < n; i++)
+	for (int i = -n + 1; i < n; i++)
 	{
 		int absI = (i < 0) ? -i : i, diff = n - absI;
 
 		/* Numbers on the left */
-		for(int j = 1; j <= diff; j++)
+		for (int j = 1; j <= diff; j++)
 			cout << j << " ";
 
 		/* Spaces/Line in the middle */
 		if (i == 0) 
 			middleSpacer = '-';
 
-		for(int j = 5 * n - 4 * diff; j > 0; j--)
+		for (int j = 5 * n - 4 * diff; j > 0; j--)
 			cout << middleSpacer;
 
 		if (i == 0)
 			middleSpacer = ' ';
 
 		/* Number on the right */
-		for(int j = diff; j > 0; j--)
+		for (int j = diff; j > 0; j--)
 			cout << " " << j;
 
 		cout << endl;
