5fd50da2d856b972b21903c8dfcaa1d2c58c884f
Angel Dimitriev <achi_d@abv.bg>
2021-11-08 16:32:31 +0200

    Update task16_sem3.cpp

--- Sem. 05/task16_sem3.cpp
+++ Sem. 05/task16_sem3.cpp
@@ -1,58 +1,68 @@
 #include <iostream>
 using namespace std;
 
+
 int main()
 {
 	int n;
 	cin >> n;
 
-	int spacesCount = 2 * (n - 1) + n + 2 * (n - 1);
-	int toGuess;
-	for (int i = 1; i <= n - 1; i++)
-	{
-		for (int j = 1; j <= i; j++)
-			cout << j << " ";
 
-		for (int j = 0; j < spacesCount; j++)
-			cout << " ";
-
-		for (int j = i; j >= 1; j--)
-			cout << " " << j;
-
-		spacesCount -= 4;
-
-		cout << endl;
-
-	}
-
-	//mid
-	for (int i = 1; i <= n; i++)
-		cout << i << " ";
-	for (int i = 0; i < n; i++)
-		cout << '-';
-	for (int i = n; i >= 1; i--)
-		cout << " " << i;
+	//up
+	for (int i = 0; i < n + 2; i++)
+		cout << '#';
 	cout << endl;
 
 
-	//down
-	spacesCount += 4;
-	for (int i = n - 1; i >= 0; i--)
+	for (int i = 0; i < n; i++)
 	{
-		for (int j = 1; j <= i; j++)
-			cout << j << " ";
+		cout << '#';
 
-		for (int j = 0; j < spacesCount; j++)
-			cout << " ";
+		for (int j = i + 1; j <= n + i; j++)
+		{
+			if (j > n)
+				cout << j % n;
+			else
+				cout << j;
 
-		for (int j = i; j >= 1; j--)
-			cout << " " << j;
+		}
+		cout << '#' << endl;
+	}
+	//mid
 
-		spacesCount += 4;
+	cout << "#";
 
-		cout << endl;
-
+	for (int i = 0; i < n; i++)
+	{
+		if (n % 2 == 1 && n / 2 == i)
+			cout << "x";
+		else if (n % 2 == 0 && ((n / 2 == i) || (n / 2 - 1 == i)))
+			cout << "x";
+		else
+			cout << ' ';
 	}
 
+	cout << '#' << endl;
 
-} 
+
+	//down
+	for (int i = n - 1; i >= 0; i--)
+	{
+		cout << '#';
+
+		for (int j = i + 1; j <= n + i; j++)
+		{
+			if (j > n)
+				cout << j % n;
+			else
+				cout << j;
+
+		}
+		cout << '#' << endl;
+	}
+
+	for (int i = 0; i < n + 2; i++)
+		cout << '#';
+	cout << endl;
+}
+
