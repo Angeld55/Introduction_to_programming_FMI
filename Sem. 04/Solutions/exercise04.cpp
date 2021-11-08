8925c6792026155854207098e5bbfbe3ee79766b
reni.korueva <reni.korueva11@gmail.com>
2021-11-08 19:36:28 +0200

    Remove commented code

--- Sem. 04/Solutions/exercise04.cpp
+++ Sem. 04/Solutions/exercise04.cpp
@@ -1,61 +1,61 @@
-//#include <iostream>
-//
-//bool isPrime(int number)
-//{
-//    double temp = sqrt(number);
-//    if (number < 2)
-//    {
-//        return false;
-//    }
-//    for (size_t i = 2; i <= temp; i++)
-//    {
-//        if (number % i == 0)
-//        {
-//            return false;
-//        }
-//    }
-//    return true;
-//}
-//
-//int countPrimeDivisors(int number)
-//{
-//    int count = 0;
-//    for (size_t div = 2; div <= number; div++)
-//    {
-//        if (number % div == 0 && isPrime(div))
-//        {
-//            count++;
-//        }
-//    }
-//    return count;
-//}
-//
-//void primeDivisors(int a, int b, int k)
-//{
-//    for (size_t i = a; i <= b; i++)
-//    {
-//        if (countPrimeDivisors(i) == k)
-//        {
-//            std::cout << i << " ";
-//        }
-//    }
-//}
-//
-//int main()
-//{
-//    int a, b, k;
-//
-//    std::cout << "Enter start of interval: ";
-//    std::cin >> a;
-//
-//    std::cout << "Enter end of interval: ";
-//    std::cin >> b;
-//
-//    std::cout << "Enter k: ";
-//    std::cin >> k;
-//
-//    std::cout << "Numbers that have k different prime divisors: ";
-//    primeDivisors(a, b, k);
-//
-//    return 0;
-//}
+#include <iostream>
+
+bool isPrime(int number)
+{
+    double temp = sqrt(number);
+    if (number < 2)
+    {
+        return false;
+    }
+    for (size_t i = 2; i <= temp; i++)
+    {
+        if (number % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+int countPrimeDivisors(int number)
+{
+    int count = 0;
+    for (size_t div = 2; div <= number; div++)
+    {
+        if (number % div == 0 && isPrime(div))
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+void primeDivisors(int a, int b, int k)
+{
+    for (size_t i = a; i <= b; i++)
+    {
+        if (countPrimeDivisors(i) == k)
+        {
+            std::cout << i << " ";
+        }
+    }
+}
+
+int main()
+{
+    int a, b, k;
+
+    std::cout << "Enter start of interval: ";
+    std::cin >> a;
+
+    std::cout << "Enter end of interval: ";
+    std::cin >> b;
+
+    std::cout << "Enter k: ";
+    std::cin >> k;
+
+    std::cout << "Numbers that have k different prime divisors: ";
+    primeDivisors(a, b, k);
+
+    return 0;
+}
--- Sem. 04/Solutions/exercise05.cpp
+++ Sem. 04/Solutions/exercise05.cpp
@@ -1,47 +1,47 @@
-//#include <iostream>
-//
-//bool isPrime(int number)
-//{
-//    double temp = sqrt(number);
-//    if (number < 2)
-//    {
-//        return false;
-//    }
-//    for (size_t i = 2; i <= temp; i++)
-//    {
-//        if (number % i == 0)
-//        {
-//            return false;
-//        }
-//    }
-//    return true;
-//}
-//
-//void printPrimePairs(int number)
-//{
-//    bool isFound = false;
-//    for (size_t i = 2; i <= number / 2; i++)  // number/2 because of commutative property
-//    {
-//        if (isPrime(i) && isPrime(number - i))
-//        {
-//            std::cout << "Pair: " << i << "," << number - i << std::endl;
-//            isFound = true;
-//        }
-//    }
-//    if (!isFound)
-//    {
-//        std::cout << "There no such pair of numbers!" << std::endl;
-//    }
-//}
-//
-//int main()
-//{
-//    int number;
-//
-//    std::cout << "Enter a number: ";
-//    std::cin >> number;
-//
-//    printPrimePairs(number);
-//
-//    return 0;
-//}
+#include <iostream>
+
+bool isPrime(int number)
+{
+    double temp = sqrt(number);
+    if (number < 2)
+    {
+        return false;
+    }
+    for (size_t i = 2; i <= temp; i++)
+    {
+        if (number % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+void printPrimePairs(int number)
+{
+    bool isFound = false;
+    for (size_t i = 2; i <= number / 2; i++)  // number/2 because of commutative property
+    {
+        if (isPrime(i) && isPrime(number - i))
+        {
+            std::cout << "Pair: " << i << "," << number - i << std::endl;
+            isFound = true;
+        }
+    }
+    if (!isFound)
+    {
+        std::cout << "There no such pair of numbers!" << std::endl;
+    }
+}
+
+int main()
+{
+    int number;
+
+    std::cout << "Enter a number: ";
+    std::cin >> number;
+
+    printPrimePairs(number);
+
+    return 0;
+}
