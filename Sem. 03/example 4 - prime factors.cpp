e36de7b4703e62de90571452cb1a0ab05e5a92ac
Angel Dimitriev <achi_d@abv.bg>
2022-10-24 00:14:45 +0300

    Create example 4 - prime factors.cpp

--- Sem. 03/example 4 - prime factors.cpp
+++ Sem. 03/example 4 - prime factors.cpp
@@ -2,43 +2,31 @@
 #include <cmath>
 using namespace std;
 
+
 int main()
 {
 	unsigned int n;
 	cin >> n;
-
-
-	for (unsigned int i = n; i > 1; i--)
+	
+	if(n <= 1)
 	{
-		//check if i is prime
-
-		bool isPrime = true;
-		double sqrtFromNumberToCheck = sqrt(i);
-		for (int divCandidate = 2; divCandidate <= sqrtFromNumberToCheck; divCandidate++)
-		{
-			if (i % divCandidate == 0)
-			{
-				isPrime = false;
-				break; //спира вътрешния цикъл
-			}
-		}
-
-		if (!isPrime)
-			continue;
-
-		int count = 0;
-		while (n % i == 0)
-		{
-			count++;
-			n /= i;
-		}
-
-		if (count != 0)
-			cout << i << "^" << count << " ";
-		
-		if(n == 1)
-			break;
-
+		cout << "Not prime" << endl;
+		return 0;
 	}
+
+	bool isPrime = true;
+	double sqrtFromNumberToCheck = sqrt(n);
+	for (int i = 2; i <= sqrtFromNumberToCheck; i++)
+	{
+		if (n % i == 0)
+		{
+			isPrime = false; 
+			break;
+		}
+	}
+	if(isPrime)
+		cout << "Prime";
+	else
+		cout << "Not prime";
 	return 0;
 }
