ca70448f126fb68abd703b8ccce715d8ee3a90ae
Angel Dimitriev <achi_d@abv.bg>
2022-11-03 16:09:03 +0200

    Create task 04.cpp

---
+++ Sem. 04/Solutions/task 04.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+unsigned int concatDigitOnBack(unsigned int a, unsigned int digit)
+{
+    if(digit > 9)
+        return 0;
+    return a * 10 + digit;   
+}
+unsigned int reverse(unsigned int n)
+{
+    unsigned reversed = 0;
+    
+    while(n != 0)
+    {
+        (reversed *= 10) += (n % 10);
+        n/=10;
+    }
+    return reversed;
+}
+unsigned int concatNumbers(unsigned int a, unsigned int b)
+{
+    unsigned int result = a; 
+    unsigned int reversedB = reverse(b);
+    
+    while(reversedB != 0)
+    {
+        int lastDigit = reversedB % 10;
+        result = concatDigitOnBack(result, lastDigit);
+        reversedB /= 10;
+    }
+    return result;
+}
+
+int main()
+{
+    cout<<concatNumbers(123, 456);
+
+    return 0;
+}
