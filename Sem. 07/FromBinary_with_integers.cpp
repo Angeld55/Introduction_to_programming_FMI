343565afdfd5207ee01e59258b64cd30709d0f3a
Angel Dimitriev <achi_d@abv.bg>
2022-11-22 01:30:30 +0200

    Update FromBinary_with_integers.cpp

--- Sem. 07/FromBinary_with_integers.cpp
+++ Sem. 07/FromBinary_with_integers.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 using namespace std;
 
-unsigned int fromBinary(unsigned int n)
+unsigned int fromBinaryToDecimal(unsigned int n)
 {
 	unsigned int result = 0;
 	unsigned int mult = 1; //2^0
 	while (n != 0)
 	{
-		result += ((n % 2) * mult);
+		result += ((n % 10) * mult);
 		mult *= 2;
 		n /= 10;
 	}
@@ -17,7 +17,7 @@ unsigned int fromBinary(unsigned int n)
 
 int main()
 {
-    cout<< fromBinary(10111) << endl;
+    cout<< fromBinaryToDecimal(10111) << endl;
 
     return 0;
 }
