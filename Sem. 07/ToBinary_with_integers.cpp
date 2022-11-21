ad205907efc363139db23ebadfc617eef9b04404
Angel Dimitriev <achi_d@abv.bg>
2022-11-21 21:20:01 +0200

    Update ToBinary_with_integers.cpp

--- Sem. 07/ToBinary_with_integers.cpp
+++ Sem. 07/ToBinary_with_integers.cpp
@@ -1,13 +1,4 @@
-/******************************************************************************
-
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include <iostream>
-
 using namespace std;
 
 unsigned int toBinary(unsigned int n)
@@ -28,6 +19,5 @@ unsigned int toBinary(unsigned int n)
 int main()
 {
     cout<< toBinary(55) << endl;
-
     return 0;
 }
