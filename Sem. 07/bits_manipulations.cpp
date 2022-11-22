d5faba3c53a8a80d74462ebaab9d93a31c3281e7
Angel Dimitriev <achi_d@abv.bg>
2022-11-22 19:32:40 +0200

    Create bits_manipulations.cpp

---
+++ Sem. 07/bits_manipulations.cpp
@@ -0,0 +1,35 @@
+#include <iostream> 
+using namespace std;
+
+bool checkBit(unsigned int n, unsigned ind)
+{
+    if(ind > 31)
+        return false;
+    unsigned int mask = 1;
+    mask <<= ind;
+    return (mask & n) == mask;
+}
+
+unsigned makeBitZero(unsigned int n, unsigned int ind)
+{
+    unsigned int mask = 1;
+    mask <<= ind;
+    
+    mask = ~mask;
+    
+    return n & mask;
+}
+
+unsigned makeBitOne(unsigned int n, unsigned int ind)
+{
+    unsigned int mask = 1;
+    mask <<= ind;
+    
+    return n | mask;
+}
+
+int main()
+{
+
+    
+}
