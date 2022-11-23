73b38f158e7f01305eca3737e2c34a500eca9a98
Angel Dimitriev <achi_d@abv.bg>
2022-11-24 01:02:30 +0200

    Update examples_strings.cpp

--- Sem. 08/examples_strings.cpp
+++ Sem. 08/examples_strings.cpp
@@ -1,56 +1,72 @@
-
 #include <iostream>
-
 using namespace std;
+
 
 
 size_t myStrlen(const char* str)
 {
-    size_t count = 0;
-    while(*str != '\0')
-    {
-        count++;
-        str++;
-    }
-    return count;
+	if (str == nullptr)
+		return 0;
+    
+	size_t count = 0;
+	while (*str != '\0')
+	{
+		count++;
+		str++;
+	}
+	return count;
 }
 
+int myStrcmp(const char* first, const char* second)
+{
+	if (first == nullptr || second == nullptr)
+		return 0; //error
+
+	while (*first != '\0' && *second != '\0')
+	{
+		if (*first < *second)
+			return -1;
+		if(*first > *second)
+			return 1;
+		first++;
+		second++;
+	}
+
+	if (*first == '\0' && *second == '\0')
+		return 0;
+
+	return *first == '\0' ? -1 : 1;
+}
+
+// we assume that dest is big enough 
 void myStrCopy(const char* source, char* dest)
 {
-    while(*source != '\0')
-    {
-        *dest = *source;
-        source++;
-        dest++;
-    }
-    *dest = '\0';
+	if (source == nullptr || dest == nullptr)
+		return;
+
+	while (*source != '\0')
+	{
+		*dest = *source;
+		dest++;
+		source++;
+	}
+	*dest = '\0';
 }
 
-int myStrCmp(const char* first, const char* second)
+
+// we assume that dest is big enough 
+void myStrCat(char* dest, const char* source)
 {
-    while(*first != '\0' && *second != '\0')
-    {
-        if(*first > *second)
-            return 1;
-        else if(*first < *second)
-            return -1;
-        first++;
-        second++;
-    }
-    if(*first == '\0' && *second == '\0')
-        return 0;
-    
-    return *first == '\0' ? -1 : 1;
+	size_t destLen = myStrlen(dest);
+	dest += destLen;
+	myStrCopy(source, dest);
 }
 
 int main()
 {
-  char str[] = "TEST HELLO";
-  
-  char str2[2000];
-  
-  myStrCopy(str, str2);
-  
-  cout << str2;
-    
+	char str[20] = "ABC";
+	char str2[] = "XY";
+	myStrCat(str, str2);
+
+	cout << str;
 }
