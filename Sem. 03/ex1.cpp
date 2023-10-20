1774cd3511ea0b4bba52202472948b0d2374186b
Angel Dimitriev <achi_d@abv.bg>
2023-10-21 01:46:37 +0300

    Update ex1.cpp

--- Sem. 03/ex1.cpp
+++ Sem. 03/ex1.cpp
@@ -3,14 +3,14 @@
 using namespace std;
 int main()
 {
-	int n, k;
+	unsigned int n, k;
 	cin >> n >> k;
 
-	int max = n > k ? n : k;
-	int lcmCindidate = max;
+	unsigned max = n > k ? n : k;
+	unsigned lcmCindidate = max;
 	while (lcmCindidate % n != 0 || lcmCindidate % k != 0)
 	{
-		lcmCindidate++;
+		lcmCindidate += max;
 	}
 
 	cout << lcmCindidate << endl;
