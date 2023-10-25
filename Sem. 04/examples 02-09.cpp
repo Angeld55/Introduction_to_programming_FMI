d5e43b39121248e8c1fb6e6b83f80c28c8f659ad
Angel Dimitriev <achi_d@abv.bg>
2023-10-26 01:49:52 +0300

    Update examples 02-09.cpp

--- Sem. 04/examples 02-09.cpp
+++ Sem. 04/examples 02-09.cpp
@@ -122,7 +122,7 @@ unsigned int power(unsigned int n, unsigned int k)
 
 unsigned int log(unsigned int n, unsigned int k)
 {
-	unsigned int powerOfK = 0;
+	unsigned int powerOfK = 1;
 	unsigned int count = 0;
 	while (powerOfK <= n)
 	{
