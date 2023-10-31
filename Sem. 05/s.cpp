3e82f25cd760e1a966fa741b3e036550e0e2af09
Angel Dimitriev <achi_d@abv.bg>
2023-10-31 18:54:37 +0200

    Create s.cpp

---
+++ Sem. 05/s.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+unsigned concatDigitAtBack(unsigned n, unsigned digit)
+{
+	return n * 10 + digit;
+}
+
+unsigned countDigitOccurences(unsigned n, unsigned digit)
+{
+	if (digit == 0 && n == 0)
+		return 1;
+
+	unsigned count = 0;
+	while (n != 0)
+	{
+		int lastDigit = n % 10;
+		if (lastDigit == digit)
+			count++;
+		n /= 10;
+	}
+	return count;
+}
+
+unsigned concatCountTimesDigit(unsigned a, unsigned digit, unsigned count)
+{
+	unsigned result = a;
+	for (int i = 0; i < count; i++)
+		result = concatDigitAtBack(result, digit);
+	return result;
+}
+
+unsigned sortNumber(unsigned n)
+{
+	unsigned result = 0;
+	for (int i = 9; i >= 0; i--)
+	{
+		unsigned count = countDigitOccurences(n, i);
+		result = concatCountTimesDigit(result, i, count);
+	}
+	return result;
+}
+int main()
+{
+    unsigned res = sortNumber(514332);
+    
+    cout << res;
+}
