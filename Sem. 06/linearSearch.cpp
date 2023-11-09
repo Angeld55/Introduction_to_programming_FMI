792b1cc59f74d74cc54c75e740aa6e0cdcd9710a
Angel Dimitriev <achi_d@abv.bg>
2023-11-10 01:21:38 +0200

    Update linearSearch.cpp

--- Sem. 06/linearSearch.cpp
+++ Sem. 06/linearSearch.cpp
@@ -3,9 +3,9 @@ using namespace std;
 
 const int GRADES_COUNT = 5;
 
-int linearSearch(const int arr[], size_t len, int element)
+int linearSearch(const int arr[], int len, int element)
 {
-	for (size_t i = 0; i < len; i++)
+	for (int i = 0; i < len; i++)
 	{
 		if (arr[i] == element)
 			return i;
@@ -15,7 +15,7 @@ int linearSearch(const int arr[], size_t len, int element)
 
 int main()
 {
-	constexpr size_t ARR_SIZE = 5;
+	constexpr int ARR_SIZE = 5;
 	int arr[ARR_SIZE] = { 5, 4, 3, 2, 1 };
 
 	bool containsTwo = linearSearch(arr, ARR_SIZE, 2) >= 0;
