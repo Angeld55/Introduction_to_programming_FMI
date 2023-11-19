a63a01be4046ac0d7a2faa17d9f021d3c1516092
Angel Dimitriev <achi_d@abv.bg>
2023-11-19 23:28:19 +0200

    Update 1b.cpp

--- hw1_is/1b.cpp
+++ hw1_is/1b.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-unsigned foundFirstOccurenceIndex(const int arr[], size_t size, unsigned beginIndex, int element)
+int foundFirstOccurenceIndex(const int arr[], size_t size, unsigned beginIndex, int element)
 {
 	for (unsigned i = beginIndex; i < size; i++)
 		if (arr[i] == element)
@@ -16,7 +16,7 @@ bool canFirstBeFromSecondByDeletion(const int arr1[], size_t size1, const int ar
 	unsigned arr1Iter = 0;
 	for (unsigned arr2iter = 0; arr2iter < size2; arr2iter++)
 	{
-		unsigned firstIndexInFirstArr = foundFirstOccurenceIndex(arr1, size1, arr1Iter, arr2[arr2iter]);
+		int firstIndexInFirstArr = foundFirstOccurenceIndex(arr1, size1, arr1Iter, arr2[arr2iter]);
 		if (firstIndexInFirstArr == -1)
 			return false;
 		arr1Iter = firstIndexInFirstArr + 1; //start from the next
