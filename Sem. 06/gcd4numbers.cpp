81c353c5b89eea6da5bb062757ddc4530a85e225
Anelka <115350258+Anelka03@users.noreply.github.com>
2023-11-24 19:04:25 +0200

    Update gcd4numbers.cpp

--- Sem. 06/gcd4numbers.cpp
+++ Sem. 06/gcd4numbers.cpp
@@ -11,10 +11,11 @@ int gcd(int a, int b)
 	}
 	while (b != 0)
 	{
-		int temp = a % b;
-		b = a;
+		int temp = b;
+		b = a % b;
 		a = temp;
 	}
+	return a;
 }
 int gcd(int a, int b, int c, int d)
 {
