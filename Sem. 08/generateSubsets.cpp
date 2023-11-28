5ec52f154332e4d025e28c34828dd44da8d1a749
Angel Dimitriev <achi_d@abv.bg>
2023-11-28 19:59:48 +0200

    Create generateSubsets.cpp

---
+++ Sem. 08/generateSubsets.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+void printSubset(const int arr[], size_t size, unsigned mask)
+{
+	cout << "{ ";
+	for (int i = size - 1; i >= 0; i--)
+	{
+		if (mask % 2 != 0)
+			cout << arr[i] << " ";
+		mask /= 2;
+	}
+	cout << "} " << endl;
+
+}
+
+unsigned powerOfTwo(unsigned n)
+{
+	return 1 << n;
+}
+void generateSubsets(const int arr[], size_t size)
+{
+	//size <= 32
+	unsigned pt = powerOfTwo(size);
+	for (unsigned i = 0; i < pt; i++)
+	{
+		printSubset(arr, size, i);
+	}
+}
+int main()
+{
+	constexpr size_t SIZE = 5;
+	int arr[SIZE] = { 1,2,3,4,5 };
+
+	generateSubsets(arr, SIZE);
+}
