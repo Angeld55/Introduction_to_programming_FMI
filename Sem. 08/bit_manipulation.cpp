7aa2efaeef73ee3d2cb8e2eedf5195523c4bc471
Angel Dimitriev <achi_d@abv.bg>
2023-11-30 15:51:04 +0200

    Update bit_manipulation.cpp

--- Sem. 08/bit_manipulation.cpp
+++ Sem. 08/bit_manipulation.cpp
@@ -1,3 +1,6 @@
+#include <iostream>
+using namespace std;
+
 bool checkBit(unsigned int n, unsigned ind)
 {
     unsigned int mask = 1;
@@ -5,7 +8,7 @@ bool checkBit(unsigned int n, unsigned ind)
     return (mask & n) == mask;
 }
 
-unsigned makeBitZero(unsigned int n, unsigned int ind)
+unsigned makeBitZero(unsigned int n, unsigned int ind) //clear bit
 {
     unsigned int mask = 1;
     mask <<= ind;
@@ -15,7 +18,16 @@ unsigned makeBitZero(unsigned int n, unsigned int ind)
     return n & mask;
 }
 
-unsigned makeBitOne(unsigned int n, unsigned int ind)
+unsigned makeBitOne(unsigned int n, unsigned int ind) //set bit
+{
+    unsigned int mask = 1;
+    mask <<= ind;
+    
+    return n | mask;
+}
+
+
+unsigned toggleBitOne(unsigned int n, unsigned int ind)
 {
     unsigned int mask = 1;
     mask <<= ind;
