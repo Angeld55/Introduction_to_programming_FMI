d22786df3a9455e2c5ead265eb43b2bdf91496cf
Angel Dimitriev <achi_d@abv.bg>
2023-12-16 10:10:48 +0200

    Update search_in_text.cpp

--- Sem. 10/search_in_text.cpp
+++ Sem. 10/search_in_text.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 using namespace std;
 
+unsigned myStrlen(const char* str)
+{
+	if (!str)
+		return 0;
+
+	unsigned result = 0;
+	while (*str) //is the same as (*str) != '\0'
+	{
+		result++;
+		str++;
+	}
+	return result;
+}
+
 bool isPrefix(const char* pattern, const char* text)
 {
 	while (*text != '\0' && *pattern != '\0')
@@ -15,8 +29,8 @@ bool isPrefix(const char* pattern, const char* text)
 
 bool searchInText(const char* text, const char* pattern)
 {
-	size_t textLen = strlen(text);
-	size_t patternLen = strlen(pattern);
+	size_t textLen = myStrlen(text);
+	size_t patternLen = myStrlen(pattern);
 	while(patternLen <= textLen)
 	{
 		if (isPrefix(pattern, text))
