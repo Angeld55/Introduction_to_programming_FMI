ef92a10507e1032931a614f9311f509399fedd27
Angel Dimitriev <achi_d@abv.bg>
2023-12-25 17:16:55 +0200

    Update sortLower.cpp

--- Sem. 11/sortLower.cpp
+++ Sem. 11/sortLower.cpp
@@ -1,14 +1,4 @@
-
-/******************************************************************************
-
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include <iostream>
-
 using namespace std;
 
 bool isLower(char ch)
