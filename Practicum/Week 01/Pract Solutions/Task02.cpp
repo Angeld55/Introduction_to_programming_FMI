c70a3714e56d847fe6a5c7b57d6f4d8574e91627
Aneliya21 <115350258+Aneliya21@users.noreply.github.com>
2024-10-06 17:26:13 +0300

    Create Task02.cpp

---
+++ Practicum/Week 01/Pract Solutions/Task02.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int n, m;
+	cin >> n >> m;
+
+	cout << "Divison quotient: " << (n / m);
+	cout << endl; //new line
+	cout << "Divison remainder: " << (n % m);
+	
+}
