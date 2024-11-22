41e2c3a7c71a1f7dcade686dfcc72be11a4e1ba7
Angel Dimitriev <achi_d@abv.bg>
2024-11-22 14:09:05 +0200

    Update bitManipulations.cpp

--- Sem. 08/bitManipulations.cpp
+++ Sem. 08/bitManipulations.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 using namespace std;
 
+
+const int LIMIT = sizeof(unsigned) * 8;
+
 bool checkBit(unsigned int n, unsigned ind)
 {
+    if (ind >= LIMIT)
+        return false; 
+
     unsigned int mask = 1;
     mask <<= ind;
     return (mask & n) == mask;
@@ -10,33 +16,42 @@ bool checkBit(unsigned int n, unsigned ind)
 
 unsigned makeBitZero(unsigned int n, unsigned int ind) //clear bit
 {
+    if (ind >= LIMIT)
+        return false;
+
     unsigned int mask = 1;
     mask <<= ind;
-    
+
     mask = ~mask;
-    
+
     return n & mask;
 }
 
 unsigned makeBitOne(unsigned int n, unsigned int ind) //set bit
 {
+    if (ind >= LIMIT)
+        return false;
+
     unsigned int mask = 1;
     mask <<= ind;
-    
+
     return n | mask;
 }
 
 
 unsigned toggleBitOne(unsigned int n, unsigned int ind)
 {
+    if (ind >= LIMIT)
+        return false;
+
     unsigned int mask = 1;
     mask <<= ind;
-    
+
     return n ^ mask;
 }
 
 int main()
 {
 
-    
+
 }
