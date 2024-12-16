a4e23173573b49d237175a80fe83b96cad7d83da
Aneliya21 <anelka.k.03@gmail.com>
2024-12-16 14:34:18 +0200

    solutions to additional tasks added

--- Practicum/Week 10/Solutions_AdditionalTasks/task_01_getCountOfOccurs.cpp
+++ Practicum/Week 10/Solutions_AdditionalTasks/task_01_getCountOfOccurs.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 using namespace std;
 
+int getCountOfOccurs(const char* str, char ch) {
+    if (!str) {
+        return -1;
+    }
+    int count = 0;
+
+    while (*str) {
+        if (*str == ch) {
+            count++;
+        }
+        str++;
+    }
+    return count;
+}
+
 int main() {
-    
+
 }
--- Practicum/Week 10/Solutions_AdditionalTasks/task_06_getIndexOfFirstOccur.cpp
+++ Practicum/Week 10/Solutions_AdditionalTasks/task_06_getIndexOfFirstOccur.cpp
@@ -1,6 +1,47 @@
 #include <iostream>
 using namespace std;
 
+size_t strLen(const char* str) {
+    if (!str) {
+        return 0;
+    }
+    size_t len = 0;
+    while (*str) {
+        len++;
+        str++;
+    }
+    return len;
+}
+
+int findFirstOccurrence(const char* text, const char* word) {
+    size_t textLength = strLen(text);
+    size_t wordLength = strLen(word);
+
+    if (wordLength == 0 || textLength == 0 || wordLength > textLength) {
+        return -1;
+    }
+
+    for (int i = 0; i <= textLength - wordLength; ++i) {
+        bool found = true;
+
+        for (int j = 0; j < wordLength; ++j) {
+            if (text[i + j] != word[j]) {
+                found = false;
+                break;
+            }
+        }
+
+        if (found) {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
 int main() {
-    
+    const char text[] = "Hello my friend!";
+    const char word[] = "my";
+
+    cout << findFirstOccurrence(text, word);
 }
