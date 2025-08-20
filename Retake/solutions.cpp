ca7ec3fec1688e4ed791ba45836c2b7c592f189f
Angel Dimitriev <achi_d@abv.bg>
2025-08-20 16:04:14 +0300

    Update solutions.cpp

--- Retake/solutions.cpp
+++ Retake/solutions.cpp
@@ -8,7 +8,7 @@ void print(int n)
         return;
     for (int i = 0; i < n; i++) //rows
     {
-        int procenctCount = (i < (n + 1) / 2) ?  ((i + 1) * 2 - 1) : ((n - i) * 2 - 1);
+        int procenctCount = (i < n / 2) ?  ((i + 1) * 2 - 1) : ((n - i) * 2 - 1); //two cases: first n/2 rows and the second n/2 rows
         int spacesCount = 2*n - 2 * procenctCount;
         for (int i = 0; i < procenctCount; i++)
             cout << "%";
@@ -38,7 +38,7 @@ bool isPalindrome(const int* arr, size_t size)
     return true;
 }
 bool isConcatPalindrome(const int* arr1, size_t size1, const int* arr2, size_t size2)
-{
+{ 
     int* concated = concat(arr1, size1, arr2, size2);
     bool result = isPalindrome(concated, size1 + size2);
     delete[] concated;
@@ -77,7 +77,7 @@ char* extractLetters(const char* str)
 //tests
 int main()
 {
-    print(3);
+    print(5);
 
     int arr[] = { 1,2,3,2 };
     int arr2[] = { 1 };
